0156cbf737d252ad3c9594ea5f23818e88344190
Hernan-99 <hernan24744@gmail.com>
2026-02-20 19:53:40 -0300

    chore: agrego el interceptor en root para que se ejecute

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { configInterceptors } from "./api/api.interceptors.ts";
 
-createRoot(document.getElementById('root')!).render(
+configInterceptors();
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
