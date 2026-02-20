260a8379e26ac4d2ed66e2a2540aa2d69564c665
Hernan-99 <hernan24744@gmail.com>
2026-02-20 20:22:44 -0300

    chore: uso de mensaje personalizado para carga, errores, etc

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,28 @@
 import "./App.css";
 import { PositionList } from "./components/PositionList";
+import { Message } from "./components/ui/Message";
 import { useCandidate } from "./hooks/useCandidate";
 
 function App() {
   const email = import.meta.env.VITE_EMAIL;
-  const { candidate, loading } = useCandidate(email);
+  const { candidate, loading, error } = useCandidate(email);
 
   if (loading) return <p>Cargando candidato...</p>;
   if (!candidate) return <p>Error al cargar datos del candidato</p>;
 
   return (
     <div style={{ maxWidth: 600, margin: "40px auto" }}>
-      <h1>
-        Bienvenido {candidate.firstName} {candidate.lastName}, esta es nuestra
-        lista de empleos disponibles
-      </h1>
-      <PositionList candidate={candidate} />
+      <Message loading={loading} error={error} loadingText="Cargando...">
+        {candidate && (
+          <>
+            <h1>
+              Bienvenido {candidate.firstName} {candidate.lastName}, esta es
+              nuestra lista de empleos disponibles
+            </h1>
+            <PositionList candidate={candidate} />
+          </>
+        )}
+      </Message>
     </div>
   );
 }
