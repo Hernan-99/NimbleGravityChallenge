12fc14d18a293eb4a123f189c3d9d66bcebc4418
Hernan-99 <hernan24744@gmail.com>
2026-02-20 19:24:44 -0300

    feat: integro custom hook para mostrar mi nombre y apellido

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import "./App.css";
 import { PositionList } from "./components/PositionList";
+import { useCandidate } from "./hooks/useCandidate";
 
 function App() {
+  const email = import.meta.env.VITE_EMAIL;
+  const { candidate, loading } = useCandidate(email);
+
+  if (loading) return <p>Cargando candidato...</p>;
+  if (!candidate) return <p>Error al cargar datos del candidato</p>;
+
   return (
-    <div>
-      <h1>Bienvenido, esta es nuestra lista de empleos disponibles</h1>
+    <div style={{ maxWidth: 600, margin: "40px auto" }}>
+      <h1>
+        Bienvenido {candidate.firstName} {candidate.lastName}, esta es nuestra
+        lista de empleos disponibles
+      </h1>
       <PositionList />
     </div>
   );
