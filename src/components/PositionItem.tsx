7003ad440ae141b107a0ca145630e32294360a92
Hernan-99 <hernan24744@gmail.com>
2026-02-20 20:05:28 -0300

    feat: implemento custom hook en componentes y manejo de envio de aplicacion

--- src/components/PositionItem.tsx
+++ src/components/PositionItem.tsx
@@ -1,15 +1,58 @@
-export const PositionItem = () => {
+import { applyJob } from "@/services/candidate.service";
+import type { Candidate } from "@/types/candidate.types";
+import type { Job } from "@/types/job.types";
+import { useState } from "react";
+
+interface PositionIProps {
+  position: Job;
+  candidate: Candidate;
+}
+
+export const PositionItem = ({ position, candidate }: PositionIProps) => {
+  const [repoUrl, setRepoUrl] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [success, setSuccess] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  const handleSubmit = async () => {
+    try {
+      setLoading(true);
+      setError(null);
+      setSuccess(false);
+
+      await applyJob({
+        uuid: candidate.uuid,
+        candidateId: candidate.candidateId,
+        jobId: position.id,
+        repoUrl,
+      });
+
+      setSuccess(true);
+    } catch (err: unknown) {
+      setError(
+        err instanceof Error ? err.message : "Error al aplicar al empleo",
+      );
+    }
+  };
   return (
     <div style={{ border: "1px solid #ccc", padding: 16, marginBottom: 16 }}>
-      <h3>Posiciones</h3>
+      <h3>{position.title}</h3>
 
       <input
         type="text"
         placeholder="Ingresar url del repositorio"
+        value={repoUrl}
+        onChange={(e) => setRepoUrl(e.target.value)}
         style={{ width: "100%", marginBottom: 8 }}
       />
 
-      <button>Enviar</button>
+      <button onClick={handleSubmit} disabled={loading || !repoUrl}>
+        {loading ? "Enviando..." : "Enviar"}
+      </button>
+
+      {success && <p style={{ color: "green" }}>Aplicacion exitosa!</p>}
+
+      {error && <p style={{ color: "green" }}>{error}</p>}
     </div>
   );
 };
--- src/components/PositionList.tsx
+++ src/components/PositionList.tsx
@@ -1,9 +1,25 @@
+import type { Candidate } from "@/types/candidate.types";
 import { PositionItem } from "./PositionItem";
+import { usePositionJobs } from "@/hooks/useJobs";
 
-export const PositionList = () => {
+interface JobLProps {
+  candidate: Candidate;
+}
+
+export const PositionList = ({ candidate }: JobLProps) => {
+  const { position, loading } = usePositionJobs();
+
+  if (loading) return <p>Cargando empleos...</p>;
+
   return (
     <div>
-      <PositionItem />
+      {position.map((position) => (
+        <PositionItem
+          key={position.id}
+          position={position}
+          candidate={candidate}
+        />
+      ))}
     </div>
   );
 };
