c6ec9bfd66c5a7304733f8c08cd4e69cea6b7490
Hernan-99 <hernan24744@gmail.com>
2026-02-20 18:54:16 -0300

    feat: implemento servicio para obtener perfil y aplicar al empleo

---
+++ src/services/candidate.service.ts
@@ -0,0 +1,24 @@
+import { api } from "@/api/api.axios";
+import type { Candidate } from "@/types/candidate.types";
+
+export const getCandidateByEmail = async (
+  email: string,
+): Promise<Candidate> => {
+  const { data } = await api.get<Candidate>(`/candidate/get-by-email`, {
+    params: { email },
+  });
+  return data;
+};
+
+export const applyJob = async (payload: {
+  uuid: string;
+  candidateId: string;
+  jobId: string;
+  repoUrl: string;
+}) => {
+  const { data } = await api.post<{ ok: boolean }>(
+    `/candidate/apply-to-job`,
+    payload,
+  );
+  return data;
+};
