1b04c188eb35f026c3a1c558ec440e3b5f971465
Hernan-99 <hernan24744@gmail.com>
2026-02-20 22:11:56 -0300

    feat: obtengo el mensaje desde el backend para que sea mas especifico

--- src/api/api.interceptors.ts
+++ src/api/api.interceptors.ts
@@ -4,7 +4,13 @@ export const configInterceptors = () => {
   api.interceptors.response.use(
     (res) => res,
     (err) => {
+      const data = err.response?.data;
+      const backendMessage =
+        data?.details?.fieldErrors &&
+        Object.values(data.details.fieldErrors).flat().join(", ");
+
       const message =
+        backendMessage ||
         err.response?.data?.message ||
         err.message ||
         "Ups ocurrio un error inesperado";
