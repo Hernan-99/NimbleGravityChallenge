53f6de25efa95c7e1ad4e2de72dc2fcc228b9ede
Hernan-99 <hernan24744@gmail.com>
2026-02-20 21:31:11 -0300

    feat: uso del componente reuitilizable para los mensajes de carga, error, etc

--- src/components/PositionList.tsx
+++ src/components/PositionList.tsx
@@ -1,25 +1,29 @@
 import type { Candidate } from "@/types/candidate.types";
 import { PositionItem } from "./PositionItem";
 import { usePositionJobs } from "@/hooks/useJobs";
+import { Message } from "./ui/Message";
 
 interface JobLProps {
   candidate: Candidate;
 }
 
 export const PositionList = ({ candidate }: JobLProps) => {
-  const { position, loading } = usePositionJobs();
-
-  if (loading) return <p>Cargando empleos...</p>;
+  const { position, loading, error } = usePositionJobs();
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-6 p-2 md:p-8 lg:p-14">
-      {position.map((position) => (
-        <PositionItem
-          key={position.id}
-          position={position}
-          candidate={candidate}
-        />
-      ))}
-    </div>
+    <Message 
+    loading={loading} 
+    error={error} 
+    loadingText="Cargando empleos...">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-6 p-2 md:p-8 lg:p-14">
+        {position.map((position) => (
+          <PositionItem
+            key={position.id}
+            position={position}
+            candidate={candidate}
+          />
+        ))}
+      </div>
+    </Message>
   );
 };
